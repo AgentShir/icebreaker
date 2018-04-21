fb7fd4206bbcf6fefec76f5f1553131c7c46b1a2
Shireen Garcia <garciasal@gmail.com>
2018-04-21 03:05:35 -0700

    Testing db rules

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import About from './About.js';
 import Admin from './Admin.js';
 import Top from './Top.js';
 import Footer from './Footer.js';
+import SignIn from './SignIn.js';
 
 // Styles
 import '../styles/App.css';
@@ -22,6 +23,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/admin" component={Admin} />
+            <Route path="/signin" component={SignIn} />
             <Route path="/top" component={Top} />
             <Route path="/footer" component={Footer} />
           </Switch>
