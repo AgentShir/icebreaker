39011c7ad2813bff8e85dc71177a00fed88f36d4
Shireen Garcia <garciasal@gmail.com>
2018-04-27 03:04:31 -0700

    Updating dependencies

--- src/components/CurrentUser.js
+++ src/components/CurrentUser.js
@@ -1,2 +1,31 @@
-import React, { PropTypes } from 'react';
-import { auth } from '../base';
+import React from 'react';
+import PropTypes from 'prop-types';
+import IcebreakerForm from './IcebreakerForm.js';
+
+// Semantic UI React Components
+import { Image, Header } from 'semantic-ui-react';
+
+// Styles
+import '../styles/App.css';
+
+const CurrentUser = ({ user }) => {
+  return (
+    <Header as='h3'>
+        <Image circular src={ user.photoURL } alt={ user.displayName } />
+              { user.displayName } is logged in.
+        <Header color='blue' as='h1' textAlign='center'>Input Icebreaker Here</Header>
+        <IcebreakerForm />
+    </Header>
+  )
+}
+
+CurrentUser.propTypes = {
+  user: PropTypes.shape({
+    displayName: PropTypes.string,
+    email: PropTypes.string.isRequired,
+    photoURL: PropTypes.string,
+    uid: PropTypes.string.isRequired
+  })
+};
+
+export default CurrentUser;
