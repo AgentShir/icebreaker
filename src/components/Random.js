83143b67304d9162ca533aff1846c724bf6f7810
Shireen Garcia <garciasal@gmail.com>
2018-06-20 03:14:47 -0700

    Wiring up the database in Random

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 
+// Database
+import base from '../base';
+
 // Semantic UI React Components
 import { Button, Container } from 'semantic-ui-react';
 
+// Lodash
+import sample from 'lodash/sample';
+
 
 class Random extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      icebreaker: null
+    }
+    this.breakerRef = base.database().ref('/icebreakers');
+  }
+
+  componentDidMount() {
+    this.breakerRef.on('value', (snapshot) => {
+      this.setState({ icebreaker: snapshot.val() })
+    })
+  }
+
+  onClick() {
+    // Using sample function from lodash
+    sample(this.breakerRef);
+    console.log('A thing is happening!');
+  }
+
   render() {
     return (
       <Container style={{ padding: '1.5em'}} textAlign='center'>
-        <Button basic color='blue'>
+        <Button onClick={this.onClick} basic color='blue'>
           Click Here To Get A Random Icebreaker
         </Button>
-        <div>Icebreaker should display here, should be new one each time the button is clicked</div>
+        <div><h1>Things will display here</h1></div>
       </Container>
     )
   }
